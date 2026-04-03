2984a7b0ae9c0868cb8f20202ad42da2f64c9016
mcmarin21 <candresm2200@alumno.ipn.mx>
2026-04-02 21:21:36 -0600

    Eliminar codigo de ejemplo

--- composeApp/src/commonMain/kotlin/com/github/mcmarin21/skellar/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/mcmarin21/skellar/app/App.kt
@@ -24,27 +24,6 @@ import skellar_app.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     SkellarTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.surface)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting", color = MaterialTheme.colorScheme.onSurface)
-                }
-            }
-        }
+        
     }
 }
