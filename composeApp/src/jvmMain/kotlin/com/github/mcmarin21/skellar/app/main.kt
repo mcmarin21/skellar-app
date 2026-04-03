ab79085d3e8ac65a7a57f58eb5af9dadbeb79ef7
mcmarin21 <candresm2200@alumno.ipn.mx>
2026-04-03 10:29:49 -0600

    Eliminacion de dependencias en el main e inicializacion maximizada de la app

--- composeApp/src/jvmMain/kotlin/com/github/mcmarin21/skellar/app/main.kt
+++ composeApp/src/jvmMain/kotlin/com/github/mcmarin21/skellar/app/main.kt
@@ -1,12 +1,15 @@
 package com.github.mcmarin21.skellar.app
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Skellar App",
+        state = rememberWindowState(placement = WindowPlacement.Maximized)
     ) {
         App()
     }
